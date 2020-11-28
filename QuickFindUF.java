7549bd08843220d8109d23c2888f41fb866aaf98
Subhayan Ghosh <subhayanghosh2424@gmail.com>
2020-11-28 22:58:49 +0530

    Weighted Quick Union with Path Compression

--- QuickFindUF.java
+++ QuickFindUF.java
@@ -40,7 +40,10 @@ public class QuickFindUF
     private int root(int i)
     {
         while (i != id[i])
+        {
+            id[i] = id[id[i]];
             i = id[i];
+        }
         return i;
     }
 
